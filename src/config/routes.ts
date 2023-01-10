c6d6b6a753997875dc9df4d709c58bc2a10480ce
Kaushal Shah <kaushal.s@auberginesolutions.com>
2023-01-11 00:40:31 +0530

    hide breakpoint

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -2,3 +2,5 @@ export const ROUTES = {
   PAGE1: '/page1',
   PAGE2: '/page2',
 };
+
+export const showBreakpoint = false;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,23 @@
+import { showBreakpoint } from '~/config';
 import type { AppProps } from 'next/app';
 import '~/styles';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
-      <div
-        className='fixed bottom-0 right-0 flex h-8 w-8 items-center justify-center border bg-neutral-50 p-6 font-bold opacity-75'
-        style={{ zIndex: 1000 }}
-      >
-        <div className='block sm:hidden md:hidden lg:hidden xl:hidden 2xl:hidden'>XS</div>
-        <div className='hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden'>SM</div>
-        <div className='hidden sm:hidden md:block lg:hidden xl:hidden 2xl:hidden'>MD</div>
-        <div className='hidden sm:hidden md:hidden lg:block xl:hidden 2xl:hidden'>LG</div>
-        <div className='hidden sm:hidden md:hidden lg:hidden xl:block 2xl:hidden'>XL</div>
-        <div className='hidden sm:hidden md:hidden lg:hidden xl:hidden 2xl:block'>2XL</div>
-      </div>
+      {showBreakpoint && (
+        <div
+          className='fixed bottom-0 right-0 flex h-8 w-8 items-center justify-center border bg-neutral-50 p-6 font-bold opacity-75'
+          style={{ zIndex: 1000 }}
+        >
+          <div className='block sm:hidden md:hidden lg:hidden xl:hidden 2xl:hidden'>XS</div>
+          <div className='hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden'>SM</div>
+          <div className='hidden sm:hidden md:block lg:hidden xl:hidden 2xl:hidden'>MD</div>
+          <div className='hidden sm:hidden md:hidden lg:block xl:hidden 2xl:hidden'>LG</div>
+          <div className='hidden sm:hidden md:hidden lg:hidden xl:block 2xl:hidden'>XL</div>
+          <div className='hidden sm:hidden md:hidden lg:hidden xl:hidden 2xl:block'>2XL</div>
+        </div>
+      )}
 
       <Component {...pageProps} />
     </>
